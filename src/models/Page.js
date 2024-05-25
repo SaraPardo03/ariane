0b0529913e0f16ad37de41cd907d5851e8117f88
Pardo <sarah.pardo03@gmail.com>
2024-05-26 00:27:17 +0200

    Refactored of page model to fetch data from REST API instead firebase

--- src/models/Page.js
+++ src/models/Page.js
@@ -1,68 +1,187 @@
-import { ref, push, equalTo, set, remove } from 'firebase/database';
-import { db } from '../configs/firebaseConfig';
-import {getAuth } from 'firebase/auth';
-import Story from './Story'; 
+import { getAuth } from 'firebase/auth';
+import { API_URL } from '../configs/configBDD';
+import { getToken } from '../helpers/authHelpers';
+import Story from './Story';
 
 class Page {
   constructor(data) {
+    this.id = data.id || null;
+    this.storyId = data.storyId || null;
     this.end = data.end || false;
     this.first = data.first || false;
     this.text = data.text || '';
     this.title = data.title || '';
     this.previousPageId = data.previousPageId || null;
-    this.totalCharacters = data.totalCharacters || this.text.length || 0; 
+    this.totalCharacters = data.totalCharacters || this.text.length || 0;
   }
+
   // Static method to notify Story during operations on pages
   static async notifyStory(storyId) {
     await Story.updateStats(storyId);
   }
+
   // Method to save a new page to the database
   async save(storyId) {
-    const auth = getAuth();
     const pageData = {
+      storyId:storyId,
+      previousPageId: this.previousPageId || null,
       end: this.end,
-      first: this.first,  
+      first: this.first,
       text: this.text,
       title: this.title,
-      previousPageId : this.previousPageId ,
       totalCharacters: this.text.length,
     };
-    const pagesRef = ref(db, `pages/${auth.currentUser.uid}/${storyId}`);
-    if (this.id) {
-      const pageRef = ref(pagesRef, this.id);
-      await set(pageRef, pageData);
+
+    if(this.previousPageId != null){
+      pageData['previousPageId'] = this.previousPageId;
+    }
+
+    try {
+      let response;
+      
+      response = await fetch(`${API_URL}/pages/${storyId}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${getToken()}`
+        },
+        body: JSON.stringify(pageData),
+      });
+      
+
+      if (!response.ok) {
+        throw new Error(`Error saving page: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      if (!data.page) {
+        throw new Error(`Invalid Response Format: ${response.statusText}`);
+      }
+
+      this.id = data.page.id;
       await Page.notifyStory(storyId);
       return this.id;
-    } else {
-      const newPageRef = push(pagesRef);
-      await set(newPageRef, pageData);
-      await Page.notifyStory(storyId);
-      return newPageRef.key;
+    } catch (error) {
+      console.error('Error saving page:', error);
+      throw error;
     }
   }
 
-    // Method to update a page in the database
-  async update(storyId, pageId) {
-    const auth = getAuth();
-    const pageRef = ref(db, `pages/${auth.currentUser.uid}/${storyId}/${pageId}`);
+  // Method to update a story in the database
+  async update(storyId) {
     const pageData = {
       end: this.end,
       first: this.first,
       text: this.text,
       title: this.title,
-      previousPageId: this.previousPageId,
+      previousPageId: this.previousPageId || null,
       totalCharacters: this.text.length,
     };
-    await set(pageRef, pageData);
-    await Page.notifyStory(storyId);
+    
+    if (!this.id) throw new Error('Cannot update page without an ID');
+    try {
+      const response = await fetch(`${API_URL}page/page/${this.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${getToken()}`
+        },
+        body: JSON.stringify(pageData),
+      });
+      
+      if (!response.ok) {
+        throw new Error(`Error updapting up: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      if(!data.page){
+        throw new Error(`Invalid Response Format Error: ${response.statusText}`);
+      }
+      await Page.notifyStory(storyId);
+      return data.page.id
+      
+    } catch (error) {
+        console.error('Error updating up:', error);
+      //throw error;
+    }
   }
   
   // Method to delete a page from the database
-  async delete(storyId, pageId) {
-    const auth = getAuth();
-    const pageRef = ref(db, `pages/${auth.currentUser.uid}/${storyId}/${pageId}`);
-    await remove(pageRef);
-    await Page.notifyStory(storyId);
+  async delete() {
+    if (!this.id) throw new Error('Cannot delete page without an ID');
+
+    try {
+      const response = await fetch(`${API_URL}/pages/page/${this.id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${getToken()}`
+        }
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error deleting page: ${response.statusText}`);
+      }
+
+      await Page.notifyStory(storyId);
+    } catch (error) {
+      console.error('Error deleting page:', error);
+      throw error;
+    }
+  }
+
+  // Static method to fetch pages by storyId
+  static async getPagesByStoryId(storyId) {
+    try {
+      const response = await fetch(`${API_URL}/pages/${storyId}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${getToken()}`
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error getting pages: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      if (!data.pages) {
+        throw new Error(`Invalid Response Format: ${response.statusText}`);
+      }
+
+      return data.pages.map(page => new Page(page));
+    } catch (error) {
+      console.error('Error getting pages:', error);
+      throw error;
+    }
+  }
+
+  // Static method to delete pages by storyId
+  static async deletePagesByStoryId(storyId) {
+    try {
+      const response = await fetch(`${API_URL}/pages/${storyId}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${getToken()}`
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error deleting pages: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      if (!data.pages) {
+        throw new Error(`Invalid Response Format: ${response.statusText}`);
+      }
+
+      return data.pages
+    } catch (error) {
+      console.error('Error getting pages:', error);
+      throw error;
+    }
   }
 }
 
